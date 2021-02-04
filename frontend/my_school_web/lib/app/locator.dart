1677f6eb7906c29fcc0c9800cb807cf9051610d5
Salim Benabadji <benabadji.mohammed.salim@gmail.com>
2021-02-04 12:46:17 +0100

    small refactor

--- frontend/my_school_web/lib/app/locator.dart
+++ frontend/my_school_web/lib/app/locator.dart
@@ -20,7 +20,6 @@ void setupLocator() {
   // locator.registerLazySingleton<WorkingHoursService>(() => FakeWorkingHoursService());
   locator.registerLazySingleton<ClassRoomService>(() => ClassRoomServiceImpl());
   locator.registerLazySingleton<GroupService>(() => GroupServiceImpl());
-  locator.registerLazySingleton<StudentGroupService>(() => StudentGroupServiceImpl());
   locator.registerLazySingleton<SectionService>(() => SectionServiceImpl());
   locator.registerLazySingleton<SubjectService>(() => SubjectServiceImpl());
   locator.registerLazySingleton<TeacherSubjectService>(() => TeacherSubjectServiceImpl());
